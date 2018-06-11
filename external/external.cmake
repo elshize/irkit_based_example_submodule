6a33f08bcace989cbb9d00f0d127612f2f094e9c
Michal Siedlaczek <michal.siedlaczek@nyu.edu>
2018-06-11 13:05:01 -0400

    Change irkit dependency to dev branch

--- external/external.cmake
+++ external/external.cmake
@@ -1,3 +1,3 @@
-#execute_process(COMMAND git submodule update --init external/irkit
-#                WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR})
+execute_process(COMMAND git submodule update --init -- external/irkit
+                WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR})
 add_subdirectory(external/irkit EXCLUDE_FROM_ALL)

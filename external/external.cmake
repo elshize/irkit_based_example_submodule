33007ee5c8de2168998078fc902349101b9f0dbd
Michal Siedlaczek <michal.siedlaczek@nyu.edu>
2018-06-14 14:45:56 -0400

    Add git submodule update to cmake

--- external/external.cmake
+++ external/external.cmake
@@ -1,1 +1,4 @@
+execute_process(COMMAND git submodule update --init
+                WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}/..
+                OUTPUT_QUIET)
 add_subdirectory(external/irkit EXCLUDE_FROM_ALL)

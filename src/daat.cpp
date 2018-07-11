32837a5afe21ddcd8dafd2170c3cc4034f9f47d4
Yuhong Zhang <danielzhangyh@gmail.com>
2018-07-11 16:41:54 -0400

    daat update bugs to fix

--- src/daat.cpp
+++ src/daat.cpp
@@ -40,28 +40,27 @@ auto daat(int k, const std::vector<post_list_t>& query,
             cur_posting = plist.begin().document();
         }
     }
-    int flag;
+    int flag = 0;
     long cur_score = 0;
     while(flag < pointers.size()){
+        std::cout << "debug\n";
         flag = 0;
         cur_score = 0;
         document_t min = document_t(collection_size);
         for(size_t i = 0; i < pointers.size(); ++i){
             if(pointers[i] != query[i].end()){
-                auto next = pointers[i].nextgeq(cur_posting);
-                if(next.document() == cur_posting){
-                    cur_score += next.payload();
-                    next.moveto(cur_posting + 1);
-                    min = std::min(min, next.document());
+                if(pointers[i].document() == cur_posting){
+                    std::cout << "debug\n";
+                    cur_score += pointers[i].payload();
+                    pointers[i].moveto(cur_posting + 1);
                 }
-                else{
-                    min = std::min(min, pointers[i].document());
-                }
+                min = std::min(min, pointers[i].document());
             }
             else{
                 ++flag;
             }
         }
+        std::cout << "debug\n";
         if(cur_score > threshold){
             Posting<long, long> p {long(cur_posting), cur_score};
             topk.push_back(p);
@@ -80,6 +79,52 @@ auto daat(int k, const std::vector<post_list_t>& query,
     return topk;
 }
 
-int main(){
+int main(int argc, char** argv){
+    assert(argc == 3);
+    irk::fs::path index_dir(argv[1]);
+    std::ifstream term_in(irk::index::term_map_path(index_dir).c_str());
+    std::ifstream title_in(irk::index::title_map_path(index_dir).c_str());
+    irk::inverted_index_mapped_data_source data(index_dir,
+                                                    irk::score::bm25_tag{});
+    irk::inverted_index_view index_view(&data, irk::varbyte_codec<irk::index::document_t>{},
+                                            irk::varbyte_codec<long>{},
+                                            irk::varbyte_codec<long>{});
+    
+    std::ifstream query_in(argv[2]);
+    if (!query_in) {
+		std::cerr << "Failed to open query.txt\n";
+		exit(1);
+	}
+    
+    std::string line;
+    int k = 10;
+    int num = 1;
 
+    
+    while(getline(query_in, line)){
+        std::stringstream aQuery(line);
+        std::string term;
+        std::vector<post_list_t> query_postings;
+        while(aQuery >> term){
+            auto id = index_view.term_id(term);
+            if (id.has_value()) {
+                query_postings.push_back(
+                index_view.scored_postings(id.value()));
+            }
+        }
+        if(query_postings.size() == 0){
+            std::cout << "Can't find any result for query '"
+                      << line << "'\n";
+            continue;
+        }
+        auto result = daat(k, query_postings, index_view.collection_size());
+        std::cout << "Query " << num << ": " << line 
+                  << "\nDID\t" << "Score\n";  
+        for(auto posting : result){
+            std::cout << posting.document << "\t"
+                      << posting.score << std::endl;
+        }
+
+        ++num;
+    }
 }
